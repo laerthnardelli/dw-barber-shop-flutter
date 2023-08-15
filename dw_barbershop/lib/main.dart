83feebe2b589fec1467b732d6a44f5e0e1d96f9f
laerthnardelli <laerthnardelli@gmail.com>
2023-08-15 18:26:57 -0300

    add initial config to riverpod

--- dw_barbershop/lib/main.dart
+++ dw_barbershop/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:dw_barbershop/src/barbershop_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
-    const BarbershopApp(),
+    const ProviderScope(child: BarbershopApp()),
   );
 }
