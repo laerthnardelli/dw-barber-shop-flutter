7f963e16ddc421701e5277e02117de05a86a8b56
laerthnardelli <laerthnardelli@gmail.com>
2023-08-15 22:00:31 -0300

    add config theme app, font and colors

--- dw_barbershop/lib/src/barbershop_app.dart
+++ dw_barbershop/lib/src/barbershop_app.dart
@@ -2,7 +2,8 @@ import 'package:asyncstate/widget/async_state_builder.dart';
 import 'package:dw_barbershop/src/features/auth/login/login_page.dart';
 import 'package:flutter/material.dart';
 
-import 'core/widgets/barbershop_loader.dart';
+import 'core/ui/widgets/barbershop_loader.dart';
+import 'core/ui/barbershop_theme.dart';
 import 'features/splash/splash_page.dart';
 
 class BarbershopApp extends StatelessWidget {
@@ -15,6 +16,7 @@ class BarbershopApp extends StatelessWidget {
       builder: (asyncNavigatorObserver) {
         return MaterialApp(
           title: 'DW Barbershop',
+          theme: BarbershopTheme.themeData,
           navigatorObservers: [asyncNavigatorObserver],
           routes: {
             '/': (_) => const SplashPage(),
--- dw_barbershop/lib/src/core/ui/widgets/barbershop_loader.dart
+++ dw_barbershop/lib/src/core/ui/widgets/barbershop_loader.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
+import '../constants.dart';
+
 class BarbershopLoader extends StatelessWidget {
   const BarbershopLoader({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: LoadingAnimationWidget.threeArchedCircle(
-      color: Colors.brown,
-      size: 60,
-    ));
+      child: LoadingAnimationWidget.threeArchedCircle(
+        color: ColorsConstants.brown,
+        size: 60,
+      ),
+    );
   }
 }
