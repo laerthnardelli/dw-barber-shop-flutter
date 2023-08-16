bfd638f1b0bf2aac4a209911d31efdd966bdb0d5
laerthnardelli <laerthnardelli@gmail.com>
2023-08-15 22:18:07 -0300

    add application provider to RestClient with riverpod

---
+++ dw_barbershop/lib/src/core/providers/application_providers.dart
@@ -0,0 +1,9 @@
+import 'package:dw_barbershop/src/core/restClient/rest_client.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'application_providers.g.dart';
+
+@Riverpod(keepAlive: true)
+RestClient restClient(RestClientRef ref) => RestClient();
+
+// dart run build_runner watch -d
