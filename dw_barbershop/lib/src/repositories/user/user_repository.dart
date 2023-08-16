73f2fed553efc4d98e7fd063da51d2a8c97cff88
laerthnardelli <laerthnardelli@gmail.com>
2023-08-16 14:37:52 -0300

    added UserRepository

---
+++ dw_barbershop/lib/src/repositories/user/user_repository.dart
@@ -0,0 +1,7 @@
+import 'package:dw_barbershop/src/core/exceptions/auth_exception.dart';
+
+import '../../core/fp/either.dart';
+
+abstract interface class UserRepository {
+  Future<Either<AuthException, String>> login(String email, String password);
+}
