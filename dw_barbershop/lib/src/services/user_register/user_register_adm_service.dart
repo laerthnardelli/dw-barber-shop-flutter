1fb35af48fe86e0777078b0ece2f20cf50349c8e
laerthnardelli <laerthnardelli@gmail.com>
2023-08-19 17:12:02 -0300

    renamed to UserRegisterAdmService

--- dw_barbershop/lib/src/services/user_register/user_register_adm_service.dart
+++ dw_barbershop/lib/src/services/user_register/user_register_adm_service.dart
@@ -3,7 +3,7 @@ import 'package:dw_barbershop/src/core/fp/nil.dart';
 
 import '../../core/fp/either.dart';
 
-abstract interface class UserRegisterService {
+abstract interface class UserRegisterAdmService {
   Future<Either<ServiceException, Nil>> execute(
       ({String name, String email, String password}) userData);
 }
