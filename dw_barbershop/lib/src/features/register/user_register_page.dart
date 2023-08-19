05e8229586133a50a2b93ca0aa769105decf2b4a
laerthnardelli <laerthnardelli@gmail.com>
2023-08-19 12:32:24 -0300

    add design to UserRegisterPage

--- dw_barbershop/lib/src/features/register/user_register_page.dart
+++ dw_barbershop/lib/src/features/register/user_register_page.dart
@@ -7,9 +7,48 @@ class UserRegisterPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register '),
+        title: const Text('Criar Conta '),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: SingleChildScrollView(
+            child: Column(
+          children: [
+            const SizedBox(height: 20),
+            TextFormField(
+              decoration: const InputDecoration(
+                label: Text('Nome'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            TextFormField(
+              decoration: const InputDecoration(
+                label: Text('E-mail'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            TextFormField(
+              decoration: const InputDecoration(
+                label: Text('Senha'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            TextFormField(
+              decoration: const InputDecoration(
+                label: Text('Confirmar Senha'),
+              ),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(56),
+              ),
+              child: const Text('Criar Conta'),
+            )
+          ],
+        )),
+      ),
     );
   }
 }
