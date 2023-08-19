ddc5a13d4436bd80b100846e15342bd088681d7b
laerthnardelli <laerthnardelli@gmail.com>
2023-08-19 17:56:28 -0300

    add integration to UserRegisterPage

--- dw_barbershop/lib/src/features/register/user_register_page.dart
+++ dw_barbershop/lib/src/features/register/user_register_page.dart
@@ -1,10 +1,52 @@
+import 'package:dw_barbershop/src/core/ui/helpers/form_helper.dart';
+import 'package:dw_barbershop/src/core/ui/helpers/messages.dart';
+import 'package:dw_barbershop/src/features/register/user_register_vm.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:validatorless/validatorless.dart';
 
-class UserRegisterPage extends StatelessWidget {
+class UserRegisterPage extends ConsumerStatefulWidget {
   const UserRegisterPage({super.key});
 
   @override
+  ConsumerState<UserRegisterPage> createState() => _UserRegisterPageState();
+}
+
+class _UserRegisterPageState extends ConsumerState<UserRegisterPage> {
+  final formKey = GlobalKey<FormState>();
+
+  final nameEC = TextEditingController();
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    nameEC.dispose();
+    emailEC.dispose();
+    passwordEC.dispose();
+
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final userRegisterVm = ref.watch(userRegisterVmProvider.notifier);
+
+    ref.listen(userRegisterVmProvider, (_, state) {
+      switch (state) {
+        case UserRegisterStateStatus.initial:
+          break;
+        case UserRegisterStateStatus.success:
+          Navigator.of(context).pushNamed('/auth/register/barbershop');
+          break;
+        case UserRegisterStateStatus.error:
+          Messages.showError(
+            'Erro ao registrar usuário administrador',
+            context,
+          );
+      }
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Criar Conta '),
@@ -12,41 +54,78 @@ class UserRegisterPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(30.0),
         child: SingleChildScrollView(
-            child: Column(
-          children: [
-            const SizedBox(height: 20),
-            TextFormField(
-              decoration: const InputDecoration(
-                label: Text('Nome'),
+            child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              TextFormField(
+                onTapOutside: (_) => context.unfocus(),
+                controller: nameEC,
+                validator: Validatorless.required('Nome obrigatório'),
+                decoration: const InputDecoration(
+                  label: Text('Nome'),
+                ),
               ),
-            ),
-            const SizedBox(height: 24),
-            TextFormField(
-              decoration: const InputDecoration(
-                label: Text('E-mail'),
+              const SizedBox(height: 24),
+              TextFormField(
+                onTapOutside: (_) => context.unfocus(),
+                controller: emailEC,
+                validator: Validatorless.multiple([
+                  Validatorless.required('E-mail obrigatório'),
+                  Validatorless.email('E-mail inválido')
+                ]),
+                decoration: const InputDecoration(
+                  label: Text('E-mail'),
+                ),
               ),
-            ),
-            const SizedBox(height: 24),
-            TextFormField(
-              decoration: const InputDecoration(
-                label: Text('Senha'),
+              const SizedBox(height: 24),
+              TextFormField(
+                onTapOutside: (_) => context.unfocus(),
+                controller: passwordEC,
+                validator: Validatorless.multiple([
+                  Validatorless.required('Senha obrigatória'),
+                  Validatorless.min(6, 'Senha deve ter no minimo 6 caracteres')
+                ]),
+                obscureText: true,
+                decoration: const InputDecoration(
+                  label: Text('Senha'),
+                ),
               ),
-            ),
-            const SizedBox(height: 24),
-            TextFormField(
-              decoration: const InputDecoration(
-                label: Text('Confirmar Senha'),
+              const SizedBox(height: 24),
+              TextFormField(
+                onTapOutside: (_) => context.unfocus(),
+                validator: Validatorless.multiple([
+                  Validatorless.required('Confirma senha obrigatória'),
+                  Validatorless.compare(
+                      passwordEC, 'Senha diferente de confirma senha'),
+                ]),
+                obscureText: true,
+                decoration: const InputDecoration(
+                  label: Text('Confirmar Senha'),
+                ),
               ),
-            ),
-            const SizedBox(height: 24),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(56),
-              ),
-              child: const Text('Criar Conta'),
-            )
-          ],
+              const SizedBox(height: 24),
+              ElevatedButton(
+                onPressed: () {
+                  switch (formKey.currentState?.validate()) {
+                    case null || false:
+                      Messages.showError('Formulário inválido', context);
+                    case true:
+                      userRegisterVm.register(
+                        name: nameEC.text,
+                        email: emailEC.text,
+                        password: passwordEC.text,
+                      );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size.fromHeight(56),
+                ),
+                child: const Text('Criar Conta'),
+              )
+            ],
+          ),
         )),
       ),
     );
