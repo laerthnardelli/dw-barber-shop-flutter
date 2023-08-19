e632491a4ceed3ab93b8f98521c98052466021d5
laerthnardelli <laerthnardelli@gmail.com>
2023-08-19 13:01:37 -0300

    add method registerAdmin to UserRepository

--- dw_barbershop/lib/src/repositories/user/user_repository_impl.dart
+++ dw_barbershop/lib/src/repositories/user/user_repository_impl.dart
@@ -5,6 +5,7 @@ import 'package:dio/dio.dart';
 import 'package:dw_barbershop/src/core/exceptions/auth_exception.dart';
 import 'package:dw_barbershop/src/core/exceptions/repository_exception.dart';
 import 'package:dw_barbershop/src/core/fp/either.dart';
+import 'package:dw_barbershop/src/core/fp/nil.dart';
 import 'package:dw_barbershop/src/core/restClient/rest_client.dart';
 import 'package:dw_barbershop/src/model/user_model.dart';
 
@@ -55,6 +56,25 @@ class UserRepositoryImpl implements UserRepository {
       log('Invalid Json', error: e, stackTrace: s);
       return Failure(
           RepositoryException(message: 'Erro ao buscar usuário logado'));
+    }
+  }
+
+  @override
+  Future<Either<RepositoryException, Nil>> registerAdmin(
+      ({String email, String name, String password}) userData) async {
+    try {
+      await restClient.unAuth.post('/users', data: {
+        'name': userData.name,
+        'email': userData.email,
+        'password': userData.password,
+        'profile': 'ADM'
+      });
+      return Success(nil);
+    } on Exception catch (e, s) {
+      log('Erro ao registrar usuário admin', error: e, stackTrace: s);
+      return Failure(
+        RepositoryException(message: 'Erro ao registrar usuário admin'),
+      );
     }
   }
 }
