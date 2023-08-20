4b55b0c6262cfb6defab0c4edfc4e1dfe401e3b1
laerthnardelli <laerthnardelli@gmail.com>
2023-08-20 15:54:46 -0300

    added vm to EmployeeRegisterPage

--- dw_barbershop/lib/src/features/employee/register/employee_register_page.dart
+++ dw_barbershop/lib/src/features/employee/register/employee_register_page.dart
@@ -1,99 +1,159 @@
+import 'dart:developer';
+
 import 'package:dw_barbershop/src/core/ui/widgets/avatar_widget.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/hours_panel.dart';
 import 'package:dw_barbershop/src/core/ui/widgets/weekdays_panel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:validatorless/validatorless.dart';
 
-class EmployeeRegisterPage extends StatefulWidget {
+import '../../../core/providers/application_providers.dart';
+import '../../../core/ui/widgets/barbershop_loader.dart';
+import '../../../model/barbershop_model.dart';
+import 'employee_register_vm.dart';
+
+class EmployeeRegisterPage extends ConsumerStatefulWidget {
   const EmployeeRegisterPage({super.key});
 
   @override
-  State<EmployeeRegisterPage> createState() => _EmployeeRegisterPageState();
+  ConsumerState<EmployeeRegisterPage> createState() =>
+      _EmployeeRegisterPageState();
 }
 
-class _EmployeeRegisterPageState extends State<EmployeeRegisterPage> {
+class _EmployeeRegisterPageState extends ConsumerState<EmployeeRegisterPage> {
   var registerADM = false;
+
+  final formKey = GlobalKey<FormState>();
+  final nameEC = TextEditingController();
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    nameEC.dispose();
+    emailEC.dispose();
+    passwordEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final employeeRegisterVm = ref.watch(employeeRegisterVmProvider.notifier);
+    final barbershopAsncValue = ref.watch(getMyBarbershopProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Cadastrar colaborador'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Center(
-            child: Column(
-              children: [
-                const AvatarWidget(),
-                const SizedBox(height: 32),
-                Row(
-                  children: [
-                    Checkbox.adaptive(
-                        value: registerADM,
-                        onChanged: (value) {
-                          setState(() {
-                            registerADM = !registerADM;
-                          });
-                        }),
-                    const Expanded(
-                      child: Text(
-                        'Sou administrador e quero me cadastrar como colaborador',
-                        style: TextStyle(
-                          fontSize: 14,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                Offstage(
-                  offstage: registerADM,
+      body: barbershopAsncValue.when(
+        error: (error, stackTrace) {
+          log('Erro ao carregar a pagina ',
+              error: error, stackTrace: stackTrace);
+          return const Center(
+            child: Text('Erro ao carregar a pagina'),
+          );
+        },
+        loading: () => const BarbershopLoader(),
+        data: (barbershopModel) {
+          final BarbershopModel(:openingDays, :openingHours) = barbershopModel;
+          return SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Form(
+                key: formKey,
+                child: Center(
                   child: Column(
                     children: [
-                      const SizedBox(height: 24),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          label: Text('Nome'),
+                      const AvatarWidget(),
+                      const SizedBox(height: 32),
+                      Row(
+                        children: [
+                          Checkbox.adaptive(
+                              value: registerADM,
+                              onChanged: (value) {
+                                setState(() {
+                                  registerADM = !registerADM;
+                                  employeeRegisterVm
+                                      .setRegisterADM(registerADM);
+                                });
+                              }),
+                          const Expanded(
+                            child: Text(
+                              'Sou administrador e quero me cadastrar como colaborador',
+                              style: TextStyle(
+                                fontSize: 14,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      Offstage(
+                        offstage: registerADM,
+                        child: Column(
+                          children: [
+                            const SizedBox(height: 24),
+                            TextFormField(
+                              controller: nameEC,
+                              validator:
+                                  Validatorless.required('Nome obrigátorio'),
+                              decoration: const InputDecoration(
+                                label: Text('Nome'),
+                              ),
+                            ),
+                            const SizedBox(height: 24),
+                            TextFormField(
+                              controller: emailEC,
+                              validator: Validatorless.multiple([
+                                Validatorless.required('E-mail obrigátorio'),
+                                Validatorless.email('E-mail inválido')
+                              ]),
+                              decoration: const InputDecoration(
+                                label: Text('E-mail'),
+                              ),
+                            ),
+                            const SizedBox(height: 24),
+                            TextFormField(
+                              controller: passwordEC,
+                              obscureText: true,
+                              validator: Validatorless.multiple([
+                                Validatorless.required('Senha obrigátoria'),
+                                Validatorless.min(6,
+                                    'Senha deve conter pelo menos 6 caracteres'),
+                              ]),
+                              decoration: const InputDecoration(
+                                label: Text('Senha'),
+                              ),
+                            ),
+                            const SizedBox(height: 24),
+                          ],
                         ),
                       ),
-                      const SizedBox(height: 24),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          label: Text('E-mail'),
-                        ),
+                      WeekdaysPanel(
+                        enabledDays: openingDays,
+                        onDayPressed: employeeRegisterVm.addOrRemoveWorkdays,
                       ),
                       const SizedBox(height: 24),
-                      TextFormField(
-                        decoration: const InputDecoration(
-                          label: Text('Senha'),
-                        ),
+                      HoursPanel(
+                        startTime: 6,
+                        endTime: 23,
+                        onHourPressed: employeeRegisterVm.addOrRemoveWorkhours,
+                        enabledTimes: openingHours,
                       ),
                       const SizedBox(height: 24),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: const Size.fromHeight(56),
+                        ),
+                        onPressed: () {},
+                        child: const Text('CADASTRAR COLABORADOR'),
+                      ),
                     ],
                   ),
                 ),
-                WeekdaysPanel(
-                    enabledDays: const ['Seg', 'Ter', 'Sab'],
-                    onDayPressed: (String day) {}),
-                const SizedBox(height: 24),
-                HoursPanel(
-                  startTime: 6,
-                  endTime: 23,
-                  onHourPressed: (int hour) {},
-                  enabledTimes: const [6, 7, 8, 9, 16],
-                ),
-                const SizedBox(height: 24),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size.fromHeight(56),
-                  ),
-                  onPressed: () {},
-                  child: const Text('CADASTRAR COLABORADOR'),
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          );
+        },
       ),
     );
   }
