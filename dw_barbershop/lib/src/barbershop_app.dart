3c4407c94b092c987a6688c316b6bda3c0422fab
laerthnardelli <laerthnardelli@gmail.com>
2023-08-20 20:16:01 -0300

    added EmployeeSchedulePage and route

--- dw_barbershop/lib/src/barbershop_app.dart
+++ dw_barbershop/lib/src/barbershop_app.dart
@@ -6,6 +6,7 @@ import 'core/ui/barbershop_nav_global_key.dart';
 import 'core/ui/widgets/barbershop_loader.dart';
 import 'core/ui/barbershop_theme.dart';
 import 'features/employee/register/employee_register_page.dart';
+import 'features/employee/schedule/employee_schedule_page.dart';
 import 'features/home/adm/home_adm_page.dart';
 import 'features/register/barbershop/barbershop_register_page.dart';
 import 'features/register/user/user_register_page.dart';
@@ -34,6 +35,7 @@ class BarbershopApp extends StatelessWidget {
             '/home/adm': (_) => const HomeAdmPage(),
             '/home/employee': (_) => const Text('Employee'),
             '/employee/register': (_) => const EmployeeRegisterPage(),
+            '/employee/schedule': (_) => const EmployeeSchedulePage(),
             '/schedule': (_) => const SchedulePage(),
           },
         );
---
+++ dw_barbershop/lib/src/features/employee/schedule/employee_schedule_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EmployeeSchedulePage extends StatelessWidget {
+  const EmployeeSchedulePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Agenda'),
+      ),
+      body: Container(),
+    );
+  }
+}
