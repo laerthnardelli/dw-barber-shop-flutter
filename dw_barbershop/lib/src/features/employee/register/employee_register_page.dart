73ea97b634c26298fa447e46ccd56a085fc58539
laerthnardelli <laerthnardelli@gmail.com>
2023-08-20 10:48:54 -0300

    add basic structure to EmployeeRegisterPage

--- dw_barbershop/lib/src/features/employee/register/employee_register_page.dart
+++ dw_barbershop/lib/src/features/employee/register/employee_register_page.dart
@@ -1,4 +1,6 @@
 import 'package:dw_barbershop/src/core/ui/widgets/avatar_widget.dart';
+import 'package:dw_barbershop/src/core/ui/widgets/hours_panel.dart';
+import 'package:dw_barbershop/src/core/ui/widgets/weekdays_panel.dart';
 import 'package:flutter/material.dart';
 
 class EmployeeRegisterPage extends StatelessWidget {
@@ -10,12 +12,67 @@ class EmployeeRegisterPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cadastrar colaborador'),
       ),
-      body: const SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              AvatarWidget(),
-            ],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Center(
+            child: Column(
+              children: [
+                const AvatarWidget(),
+                const SizedBox(height: 32),
+                Row(
+                  children: [
+                    Checkbox.adaptive(value: true, onChanged: (value) {}),
+                    const Expanded(
+                      child: Text(
+                        'Sou administrador e quero me cadastrar como colaborador',
+                        style: TextStyle(
+                          fontSize: 14,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: [
+                    const SizedBox(height: 24),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Nome'),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('E-mail'),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Senha'),
+                      ),
+                    ),
+                    const SizedBox(height: 24),
+                  ],
+                ),
+                WeekdaysPanel(onDayPressed: (String day) {}),
+                const SizedBox(height: 24),
+                HoursPanel(
+                  startTime: 6,
+                  endTime: 23,
+                  onHourPressed: (int hour) {},
+                ),
+                const SizedBox(height: 24),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size.fromHeight(56),
+                  ),
+                  onPressed: () {},
+                  child: const Text('CADASTRAR COLABORADOR'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
